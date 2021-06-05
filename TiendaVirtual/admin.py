84070a54cfa571625d0602e7bdefea019e9b57bd
gmatias <g.matiasvillagra@gmail.com>
2021-06-05 19:38:12 -0400

    Configuracion settings.py: static + templates

--- TiendaVirtual/admin.py
+++ TiendaVirtual/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from django.db.models.base import Model
+from django.db import models
 
 # Register your models here.
 class Productor(models.Model):
@@ -12,6 +12,20 @@ class Productor(models.Model):
     telefono2 = models.Charfield(max_lenght = 20,null='true')
     bancoAsociado = models.Charfield(max_lenght = 20,null='true')
     nroCuenta = models.IntergerField(null='true')
+
+class Cliente(models.Model):
+    nombre = models.Charfield(max_lenght = 20)
+    appaterno = models.Charfield(max_lenght = 20)
+    apmaterno = models.Charfield(max_lenght = 20)
+    rut = models.charfield(max_lenght = 12)
+    telefono1 = models.Charfield(max_lenght = 20)
+    telefono2 = models.Charfield(max_lenght = 20,null='true')
+    email = models
+
+    
+
+
+
 
 
 
--- TiendaVirtual/models.py
+++ TiendaVirtual/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Productor(models.Model):
+    
+    #requisitos minimos agricultor publicitario-vendedor
+    nombre = models.Charfield(max_lenght = 20)
+    appaterno = models.Charfield(max_lenght = 20)
+    apmaterno = models.Charfield(max_lenght = 20)
+    rut = models.charfield(max_lenght = 12)
+    nombreComercial = models.charfield(max_lenght=25,null='true')
+    telefono1 = models.Charfield(max_lenght = 20)
+    telefono2 = models.Charfield(max_lenght = 20,null='true')
+    
+    #requisitos exclusivos agricultor vendedor
+    email = models.EmailField(null='true')
+    bancoAsociado = models.Charfield(max_lenght = 20,null='true')
+    nroCuenta = models.IntergerField(null='true')
+
+
+class Cliente(models.Model):
+    nombre = models.Charfield(max_lenght = 20)
+    appaterno = models.Charfield(max_lenght = 20)
+    apmaterno = models.Charfield(max_lenght = 20)
+    rut = models.charfield(max_lenght = 12)
+    telefono1 = models.Charfield(max_lenght = 20)
+    telefono2 = models.Charfield(max_lenght = 20,null='true')
+    email = models.EmailField
