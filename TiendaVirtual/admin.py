e10dc95ae0a6320a9111ece685918efec8018788
gmatias <g.matiasvillagra@gmail.com>
2021-06-05 19:52:24 -0400

    Models productor + cliente

--- TiendaVirtual/admin.py
+++ TiendaVirtual/admin.py
@@ -1,26 +1,8 @@
 from django.contrib import admin
-from django.db import models
 
 # Register your models here.
-class Productor(models.Model):
-    nombre = models.Charfield(max_lenght = 20)
-    appaterno = models.Charfield(max_lenght = 20)
-    apmaterno = models.Charfield(max_lenght = 20)
-    rut = models.charfield(max_lenght = 12)
-    nombreComercial = models.charfield(max_lenght=25,null='true')
-    telefono1 = models.Charfield(max_lenght = 20)
-    telefono2 = models.Charfield(max_lenght = 20,null='true')
-    bancoAsociado = models.Charfield(max_lenght = 20,null='true')
-    nroCuenta = models.IntergerField(null='true')
 
-class Cliente(models.Model):
-    nombre = models.Charfield(max_lenght = 20)
-    appaterno = models.Charfield(max_lenght = 20)
-    apmaterno = models.Charfield(max_lenght = 20)
-    rut = models.charfield(max_lenght = 12)
-    telefono1 = models.Charfield(max_lenght = 20)
-    telefono2 = models.Charfield(max_lenght = 20,null='true')
-    email = models
+
 
     
 
--- TiendaVirtual/models.py
+++ TiendaVirtual/models.py
@@ -2,27 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Productor(models.Model):
-    
     #requisitos minimos agricultor publicitario-vendedor
-    nombre = models.Charfield(max_lenght = 20)
-    appaterno = models.Charfield(max_lenght = 20)
-    apmaterno = models.Charfield(max_lenght = 20)
-    rut = models.charfield(max_lenght = 12)
-    nombreComercial = models.charfield(max_lenght=25,null='true')
-    telefono1 = models.Charfield(max_lenght = 20)
-    telefono2 = models.Charfield(max_lenght = 20,null='true')
+    nombre = models.CharField(max_length = 20)
+    appaterno = models.CharField(max_length = 20)
+    apmaterno = models.CharField(max_length = 20)
+    rut = models.CharField(max_length = 12)
+    nombreComercial = models.CharField(max_length=25,null='true')
+    telefono1 = models.CharField(max_length = 15)
+    telefono2 = models.CharField(max_length = 15,null='true')
     
     #requisitos exclusivos agricultor vendedor
     email = models.EmailField(null='true')
-    bancoAsociado = models.Charfield(max_lenght = 20,null='true')
-    nroCuenta = models.IntergerField(null='true')
+    bancoAsociado = models.CharField(max_length = 20,null='true')
+    nroCuenta = models.IntegerField(null='true')
 
 
 class Cliente(models.Model):
-    nombre = models.Charfield(max_lenght = 20)
-    appaterno = models.Charfield(max_lenght = 20)
-    apmaterno = models.Charfield(max_lenght = 20)
-    rut = models.charfield(max_lenght = 12)
-    telefono1 = models.Charfield(max_lenght = 20)
-    telefono2 = models.Charfield(max_lenght = 20,null='true')
+    nombre = models.CharField(max_length = 20)
+    appaterno = models.CharField(max_length = 20)
+    apmaterno = models.CharField(max_length = 20)
+    rut = models.CharField(max_length = 12)
+    telefono1 = models.CharField(max_length = 15)
+    telefono2 = models.CharField(max_length = 15,null='true')
     email = models.EmailField
